19813ccaf289e236c0221669d8f33677c3985151
Drew Crawford <drew@sealedabstract.com>
2016-03-17 00:41:45 -0500

    Hack script should build master
    Always use the master tag when hacking

--- hack-swift.sh
+++ hack-swift.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed "s/__TAG__/$tag/" build.dockerfile > Dockerfile.tagged
+sed "s/__TAG__/master/" build.dockerfile > Dockerfile.tagged
 sed -i "" "s+RUN ./utils/build-script+# RUN ./utils/build-script+" Dockerfile.tagged 
 docker build -f Dockerfile.tagged -t swift-tmp .
